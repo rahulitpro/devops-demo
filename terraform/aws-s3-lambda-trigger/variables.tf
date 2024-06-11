bdda2caefb3e7e7aeeef8ea31bd3695711e49063
rahulitpro <rahulkthakar@gmail.com>
2024-06-11 09:53:24 -0400

    s3 bucket created

--- terraform/aws-s3-lambda-trigger/variables.tf
+++ terraform/aws-s3-lambda-trigger/variables.tf
@@ -1,3 +1,8 @@
 variable "aws_s3_bucket_name" {
     description = "unique name of aws s3 bucket"
 }
+
+output "aws_s3_bucket_id" {
+    description = "unique id of aws s3 bucket"
+    value =  resource.aws_s3_bucket.aws_s3_bucket.id 
+}
