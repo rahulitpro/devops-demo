6ec44a021429ed4bf969632ef2027b4b1a73fb0e
rahulitpro <rahulkthakar@gmail.com>
2024-06-11 09:32:39 -0400

    aws-s3-lambda-trigger

--- terraform/aws-s3-lambda-trigger/main.tf
+++ terraform/aws-s3-lambda-trigger/main.tf
@@ -1,1 +1,7 @@
 ####
+
+#### create AWS s3 Bucket
+
+resource "aws_s3_bucket" "aws_s3_bucket" {
+  bucket = var.aws_s3_bucket_name
+  }
---
+++ terraform/aws-s3-lambda-trigger/variables.tf
@@ -0,0 +1,3 @@
+variable "aws_s3_bucket_name" {
+    description = "unique name of aws s3 bucket"
+}
