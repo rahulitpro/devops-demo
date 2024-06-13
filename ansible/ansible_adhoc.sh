33fa288d7d91bb7bcee0f153214831e71be65ea2
Rahul Thakar <rahulkthakar@gmail.com>
2024-06-13 08:58:42 -0400

    ansible

---
+++ ansible/ansible_adhoc.sh
@@ -0,0 +1,5 @@
+ansible -i inventory.ini -m ping all
+ansible -i inventory.ini -m shell -a "echo 'Hello World'" all
+ansible -i inventory.ini -m shell -a 'echo $TERM' all
+ansible -i inventory.ini -m setup all
+ansible -i inventory.ini -a 'sudo /sbin/reboot' all
