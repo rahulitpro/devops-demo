020948a76e25c1df041b16492b362ee1ba0cb863
Rahul Thakar <rahulkthakar@gamil.com>
2024-07-08 19:56:08 -0400

    docker

--- ansible/setup_nodes.sh
+++ ansible/setup_nodes.sh
@@ -10,3 +10,14 @@
     - jenkinsagents/vars/main.yml
   roles: 
     - jenkinsagents
+
+- name: Setup Docker on apps servers
+  hosts:
+    - apps
+  become: True
+  remote_user: ansible
+  vars_files:
+    - docker/vars/{{ ansible_os_family }}.yml
+    - docker/vars/main.yml
+  roles: 
+    - docker
